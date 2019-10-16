c1a414c36911dd2ce4836e2ac4e01f175e3fabd7
Carlos Castro <alu0100819847@ull.edu.es>
2019-10-16 20:12:16 +0200

    POST user added #1

---
+++ src/main/java/es/upm/miw/apaw_ep_themes/dtos/UserCreationDto.java
@@ -0,0 +1,80 @@
+package es.upm.miw.apaw_ep_themes.dtos;
+
+import es.upm.miw.apaw_ep_themes.exceptions.BadRequestException;
+
+public class UserCreationDto {
+    private String id;
+
+    private String name;
+
+    private String password;
+
+    private String country;
+
+    private String address;
+
+    public UserCreationDto(){}
+
+    public UserCreationDto(String name, String password, String country, String address){
+        this.name = name;
+        this.password = password;
+        this.country = country;
+        this.address = address;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getCountry(){
+        return this.country;
+    }
+
+    public void setCountry(String country){
+        this.country = country;
+    }
+
+    public String getAddress(){
+        return this.address;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public void validate() {
+        if (this.name == null || this.password == null || this.name.isEmpty()|| this.password.isEmpty()  ) {
+            throw new BadRequestException("Incomplete UserBasicDto. ");
+        }
+    }
+    @Override
+    public String toString(){
+        return "UserCreationDto{" +
+                "id='" + this.id + '\'' +
+                ", name=" + this.name +
+                ", password='" + this.password + '\'' +
+                ", country='" + this.country + '\'' +
+                ", address='" + this.address + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/es/upm/miw/apaw_ep_themes/api_controllers/UserResourceIT.java
@@ -0,0 +1,47 @@
+package es.upm.miw.apaw_ep_themes.api_controllers;
+
+import es.upm.miw.apaw_ep_themes.ApiTestConfig;
+import es.upm.miw.apaw_ep_themes.dtos.UserBasicDto;
+import es.upm.miw.apaw_ep_themes.dtos.UserCreationDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ApiTestConfig
+class UserResourceIT {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void testCreate() {
+        UserBasicDto userBasicDto = createUser("Carlos", "ccarlos", "Madrid", "Calle universidad");
+        assertEquals("Carlos", userBasicDto.getName());
+    }
+
+    @Test
+    void testCreateUserException() {
+        UserCreationDto userCreationDto = new UserCreationDto("Carlos", null, "Madrid", "Calle universidad");
+        this.webTestClient
+                .post().uri(UserResource.USERS)
+                .body(BodyInserters.fromObject(userCreationDto))
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+
+    UserBasicDto createUser(String name, String password, String country, String address) {
+        UserCreationDto userCreationDto = new UserCreationDto(name, password, country, address);
+        return this.webTestClient
+                .post().uri(UserResource.USERS)
+                .body(BodyInserters.fromObject(userCreationDto))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(UserBasicDto.class)
+                .returnResult().getResponseBody();
+    }
+}
